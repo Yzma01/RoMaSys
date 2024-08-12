13e1ff2d9d06a457934f28195ea385f63d99a167
JorgeRojas720s <jitorojas37@gmail.com>
2024-08-11 18:05:46 -0600

    some details in sign-in

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -2,31 +2,43 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen bg-background">
       <SignIn
         appearance={{
           elements: {
-            // Card customization
-            card: "bg-gray-900 p-6 rounded-lg shadow-lg",
+            rootBox: "bg-background",
+            cardBox: "bg-red-900",
+            card: "flex flex-col gap-2 pl-8 pr-8 pb-4 bg-bgGray rounded-2xl transition-transform duration-400 ease-in-out hover:scale-105 hover:border-black hover:border", // Estilos principales de la tarjeta
 
-            // Hide specific elements
-            headerTitle: "hidden",
+
+            // Ocultar elementos específicos
             headerSubtitle: "hidden",
             footer: "hidden",
             poweredBy: "hidden",
+            buttonArrowIcon: "hidden",
 
-            // Form field customization
-            formFieldInput: "rounded-lg bg-gray-800 text-white p-2",
-            formFieldLabel: "text-white mb-1",
-            formFieldErrorText: "text-red-500 mt-1",
+            form: "flex gap-0",
 
-            // Button customization
-            formButtonPrimary: "bg-gray-700 text-white p-2 rounded-lg mt-4",
-            formButtonSecondary: "bg-gray-700 text-white p-2 rounded-lg mt-2",
+            formHeaderTitle: "text-white text-center text-lg my-8", // Título del encabezado
 
-            // Header title customization
-            formHeaderTitle: "text-white text-2xl mb-4",
+            formFieldWrapper: "flex items-center justify-center gap-2 rounded-2xl p-2.5 bg-gray-900 shadow-inner", // Contenedor de campos del formulario
+
+            formFieldInput: "bg-transparent border-none outline-none w-full text-gray-300", // Campo de entrada
+
+            formFieldLabel: "hidden", // Etiquetas ocultas, ya que no están en el CSS original
+
+            formButtonPrimary:
+              "flex justify-center mt-7 bg-gray-800 text-white p-2 rounded-lg transition duration-400 ease-in-out hover:bg-black hover:text-white", // Botón primario
+
+            formButtonSecondary:
+              "flex justify-center mt-4 bg-gray-800 text-white p-2 rounded-lg transition duration-400 ease-in-out hover:bg-black hover:text-white", // Botón secundario
+
+            formButtonLink:
+              "flex justify-center mb-12 bg-gray-800 text-white p-2 rounded-lg transition duration-400 ease-in-out hover:bg-red-500 hover:text-white", // Botón adicional con estilo similar
           },
+        }}
+        signInOptions={{
+          title: "Inicio de Sesion", // Título personalizado
         }}
       />
     </div>
