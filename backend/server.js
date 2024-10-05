592eead174216ee149caa7e8c577c4bbe2b6b053
JorgeRojas720s <jitorojas37@gmail.com>
2024-10-05 16:48:17 -0600

    change to environment variables

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import router from "./apiWhatsApp/routes/links.js";
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
--- src/app/components/utils/fetch.js
+++ src/app/components/utils/fetch.js
@@ -1,5 +1,6 @@
+
 export const makeFetch = async (url, method, params, body) => {
-    const baseUrl = process.env.BASE_URL || "http://localhost:3000";
+    const baseUrl = process.env.BASE_URL; // || "http://localhost:3000";
     const apiURL = `${baseUrl + url}${
       params !== "" ? "/" + params : ""
     }`;
