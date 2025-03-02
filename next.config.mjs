285e4d3c337e28a39fea8a93b3a06b6ce7cb3350
Yzma01 <marchena.isma@gmail.com>
2025-03-02 16:59:14 -0600

    test 3

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
     apiUrl:
       process.env.NODE_ENV === "development"
         ? "http://localhost:3000"
-        : "https://ro-ma-sys.vercel.app",
+        : "https://ro-ma-sys-server.vercel.app",
   },
 
   //!Este es un proxy en el que source especifico que cuando este la ruta api, redireccione al puerto 5000
