dec8dc3af639abd93491a942c5f057099b246e97
Yzma01 <marchena.isma@gmail.com>
2025-03-02 00:18:41 -0600

    test 3

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
     apiUrl:
       process.env.NODE_ENV === "development"
         ? "http://localhost:3000"
-        : "https://romasys.netlify.app",
+        : "ro-ma-sys.vercel.app",
   },
 
   //!Este es un proxy en el que source especifico que cuando este la ruta api, redireccione al puerto 5000
