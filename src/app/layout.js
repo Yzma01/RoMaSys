a270d39f0d4dda66b1244db69591fd8abad403ef
Yzma01 <marchena.isma@gmail.com>
2025-03-01 22:39:11 -0600

    test 3

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "./components/ui/toaster";
-import PreventZoom from "./components/utils/PreventZoom"; 
 
 const inter = Inter({ subsets: ["latin"] });
 
