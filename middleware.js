0d8e6e2ef0508e8c8a0235fd5d4bc4a7bd67d8f0
Yzma01 <marchena.isma@gmail.com>
2025-03-06 12:51:09 -0600

    test protect routes

--- middleware.js
+++ middleware.js
@@ -1,14 +1,16 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const protectedRoutes = createRouteMatcher([
-  '/admin',
-  '/admin/dashboard',
-])
+const isProtectedRoute = createRouteMatcher(['/admin(.)','/api(.)'])
 
-export default clerkMiddleware((auth, req) => {
-  if(protectedRoutes(req)) auth().protect();
-});
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect()
+})
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
