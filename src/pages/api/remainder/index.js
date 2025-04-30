1a9849785c75e8f6ac5af3c5f55b1ae5ad914682
Yzma01 <marchena.isma@gmail.com>
2025-04-29 23:39:51 -0600

    trying cron job for agenda

--- src/pages/api/remainder/index.js
+++ src/pages/api/remainder/index.js
@@ -2,7 +2,12 @@
 
 import { startMessageSending } from "../backend/schedule-messages/membership-to-expire.js"
 
-export async function GET() {
-  await startMessageSending();
-  return new Response("Reminders processed", { status: 200 });
+export default async function handler(req, res) {
+  console.log("taamooss");
+  if (req.method === "GET") {
+    await startMessageSending();
+    res.status(200).send("Reminders processed");
+  } else {
+    res.status(405).end(); // Method Not Allowed
+  }
 }
